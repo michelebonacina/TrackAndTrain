197d49335187bed7e02d54ab7dbfd5d74f32a15d
Michele Bonacina <michele.bonacina@gmail.com>
2020-01-13 17:31:51 +0100

    added track list item

---
+++ src/app/track/track-list-item/track-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'tat-track-list-item',
+    templateUrl: './track-list-item.component.html',
+    styleUrls: ['./track-list-item.component.scss']
+})
+export class TrackListItemComponent implements OnInit
+{
+
+    constructor() { } // constructor
+
+    ngOnInit() { } // ngOnInit
+
+} // TrackListItem
