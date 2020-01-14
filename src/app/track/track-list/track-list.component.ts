8483bf63a10fffd0208a0a2bad58c1a6c14a4d93
Michele Bonacina <michele.bonacina@gmail.com>
2020-01-14 06:50:23 +0100

    passed track object to item list

--- src/app/track/track-list/track-list.component.ts
+++ src/app/track/track-list/track-list.component.ts
@@ -8,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class TrackListComponent implements OnInit
 {
 
-    tracks = [1, 2, 3, 4, 5];
+    tracks = [
+        {
+            id: 1,
+            date: "01-01-2020",
+            time: "12:00",
+            description: "Monte Misma",
+            activity: "Mountain Bike",
+            duration: "3:30",
+            distance: 34,
+            ascent: 600
+        },
+        {
+            id: 2,
+            date: "02-01-2020",
+            time: "12:00",
+            description: "Monte Misma",
+            activity: "Mountain Bike",
+            duration: "3:30",
+            distance: 34,
+            ascent: 600
+        },
+        {
+            id: 3,
+            date: "03-01-2020",
+            time: "12:00",
+            description: "Monte Misma",
+            activity: "Mountain Bike",
+            duration: "3:30",
+            distance: 34,
+            ascent: 600
+        },
+    ];
 
     constructor() { } // contructor
 
