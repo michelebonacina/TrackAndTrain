afcd640e154fc15d6da3ec2d03358a3d13021beb
Michele Bonacina <michele.bonacina@gmail.com>
2020-01-20 12:00:06 +0100

    add activity icon to track

--- src/app/track/track-list-item/track-list-item.component.ts
+++ src/app/track/track-list-item/track-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Track } from '../shared/track.model';
+import { CommonService } from 'src/app/common/shared/common.service';
 
 /**
  * Track list item component.
@@ -20,10 +21,17 @@ export class TrackListItemComponent implements OnInit
     @Input() track: Track;      // track to be visualized
     @Input() index: number;     // ordinal track index
 
+    icons: any = {};    // fontawesome icons
+
     /**
      * Create a new component.
+     *  @param commonService service for generic operations
      */
-    constructor() { } // constructor
+    constructor(
+        private commonService: CommonService
+    ) {
+        this.icons = this.commonService.icons;
+     } // constructor
 
     /**
      * Component initialization.
