5c535de7650eb0a7304fd0c40f9ef22b6e29b010
Michele Bonacina <michele.bonacina@gmail.com>
2020-01-31 18:17:51 +0100

    add token validation check

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -141,35 +141,43 @@ exports.authenticationMiddleware = function (request, response, next)
     {
         // get user data from token
         const user = parseToken(token);
-        // load user
-        User.findById(user.userId,
-            function (error, user)
-            {
-                // check for errors
-                if (error)
+        if (user)
+        {
+            // load user
+            User.findById(user.userId,
+                function (error, user)
                 {
-                    // error during user save
-                    return response.status(500).send({ errors: MongooseHelper.normalizeError(error.errors) });
+                    // check for errors
+                    if (error)
+                    {
+                        // error during user save
+                        return response.status(500).send({ errors: MongooseHelper.normalizeError(error.errors) });
+                    }
+                    // check user
+                    if (user)
+                    {
+                        // put user in response
+                        response.locals.user = user;
+                        next();
+                    }
+                    else
+                    {
+                        // user not found
+                        return response.status(401).send({ errors: [{ title: 'Not authenticated', details: 'You need to login to get access' }] });
+                    }
                 }
-                // check user
-                if (user)
-                {
-                    // put user in response
-                    response.locals.user = user;
-                    next();
-                }
-                else
-                {
-                    // user not found
-                    return response.status(401).send({ errors: [{ title: 'Not authenticated', details: 'You need to login to get access' }] });
-                }
-            }
-        );
+            );
+        }
+        else 
+        {
+            // token not valid
+            return response.status(401).send({ errors: [{ title: 'Not authenticated', details: 'You need to login to get access' }] });
+        }
     }
     else
     {
         // password is wrong
-        return response.status(401).send({ errors: [{ title: 'Not authenticated', details: 'You need to login to get access' }] });
+        return response.status(401).send({ errors: [{ title: 'Unauthorized', details: 'Authentication token is not valid' }] });
     }
 } // authenticationMiddleware
 
@@ -177,10 +185,21 @@ exports.authenticationMiddleware = function (request, response, next)
  * Decode and parse token for getting user data.
  * The token contains an object with user id and username.
  * @param token encoded token
- * @returns decoded token
+ * @returns decoded token, null if there're some errors
  */
 parseToken = function (token)
 {
     // decode token 
-    return jwt.verify(token.split(' ')[1], config.SECRET);
+    let decodedToken;
+    try
+    {
+        decodedToken = jwt.verify(token.split(' ')[1], config.SECRET);
+    }
+    catch (error)
+    {
+        // error decoding token
+        // TODO
+    }
+    // return decoded token
+    return decodedToken;
 } // parseToken
